b11718b9add587042e41f3cb5d5a7f50fdaefd21
tscalfaro <tscalfaro7@gmail.com>
2021-11-26 10:11:43 -0500

    added store folder with auth.js and index.js files for start

--- server/auth/index.js
+++ server/auth/index.js
@@ -14,14 +14,13 @@ router.post("/login", async (req, res, next) => {
 router.post("/signup", async (req, res, next) => {
   try {
     //this is done to protect against injection attacks, so they can't change the type
-    const { username, password, email, image, address } = req.body;
+    const { firstName, lastName, password, email} = req.body;
 
     const user = await User.create({
-      username,
+      firstName,
+      lastName,
       password,
-      email,
-      image,
-      address,
+      email
     });
     res.send({ token: await user.generateToken() });
   } catch (err) {
