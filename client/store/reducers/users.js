98517eb5ff53043994a1ebb08ba82be362c46581
trzaidi <trzaidi@gmail.com>
2021-12-03 23:01:43 -0500

    login functionality, still working

--- client/store/reducers/users.js
+++ client/store/reducers/users.js
@@ -1,33 +1,95 @@
-// import axios from 'axios'
-// import {authenticateRequest} from '../gateKeepingMiddleware'
-// //ACTION TYPES
-// const GET_USERS = 'GET_USERS';
+import axios from 'axios'
+import {authenticateRequest} from '../gateKeepingMiddleware'
 
+//ACTION TYPES
+const GET_USER = 'GET_USER';
+const CREATE_USER = 'CREATE_USER';
+const GET_HOST = 'GET_HOST';
+const CREATE_HOST = 'CREATE_HOST';
 
-// //ACTION CREATORS
-// const getUsers = users => {
-//     return {
-//         type: GET_USERS,
-//         users
-//     }
-// }
+//USER ACTION CREATORS
+const getUser = (user) => {
+    return {
+        type: GET_USER,
+        user
+    }
+}
+const createUser = (newUser) => {
+  return {
+      type: CREATE_USER,
+      newUser
+  }
+}
 
-// const getUser = user => {
-//     return {
-//         type: GET_USER,
-//         user
-//     }
-// }
+//HOST ACTION CREATORS
+const getHost = (host) => {
+  return {
+      type: GET_HOST,
+      host
+  }
+}
+const createHost = (newHost) => {
+  return {
+      type: CREATE_HOST,
+      newHost
+  }
+}
 
-// //THUNKS
+//USER THUNKS
+export const fetchSingleUser = (email, password) => async dispatch => {
+    try {
+        const { user } = await axios.get(`https://tagd-backend.herokuapp.com/api/users/${id}`)
+        dispatch(getUser(user))
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const createSingleUser = (userData) => async dispatch => {
+  try {
+      const newUser = await authenticateRequest("post", `/api/user`, userData)
+      if (newUser) {
+          dispatch(createUser(newUser))
+      }
+  } catch (error) {
+      console.log(error)
+  }
+}
 
-// export const fetchSingleUser = (id) => async dispatch => {
-//     try {
-//         const user = await authenticateRequest("get", `/api/users/${id}`)
-//         if(user){
-//             dispatch(getUser(user))
-//         }
-//     } catch (error) {
+// HOST THUNKS
+export const fetchSingleHost = (email, password) => async dispatch => {
+  try {
+      const { host } = await axios.get(`https://tagd-backend.herokuapp.com/api/hosts/${id}`)
+      dispatch(getHost(host))
+  } catch (error) {
+      console.log(error)
+  }
+}
 
-//     }
-// }
+export const createSingleHost = (hostData) => async dispatch => {
+  try {
+      const newHost = await authenticateRequest("post", `/api/host`, hostData)
+      if (newHost) {
+          dispatch(createHost(newHost))
+      }
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+//STATE
+const initialState = []
+export default usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_USER:
+            return action.user
+        case CREATE_USER:
+            return [...state, action.newUser]
+        case GET_HOST:
+            return action.host
+        case CREATE_HOST:
+            return [...state, action.newHost]
+        default:
+            return state;
+    }
+}
+
