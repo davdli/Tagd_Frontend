30cfa3c67c7711c251d6f60eb84b3655cee180a8
trzaidi <trzaidi@gmail.com>
2021-12-08 23:19:53 -0500

    Revert "carousel fixing"

--- client/components/TagCarousel.js
+++ client/components/TagCarousel.js
@@ -4,18 +4,19 @@ import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, 
 
 const DATA = [
   {
-    id: "1",
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     title: "First Item",
   },
   {
-    id: "2",
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Second Item",
   },
   {
-    id: "3",
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
     title: "Third Item",
   },
 ];
+
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
@@ -28,7 +29,7 @@ const TagCarousel = ({data}) => {
   const [selectedId, setSelectedId] = useState(null);
 
   const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#008080" : "#f9c2ff";
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
     const color = item.id === selectedId ? 'white' : 'black';
 
     return (
